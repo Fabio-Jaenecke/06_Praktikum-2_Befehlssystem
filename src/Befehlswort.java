8b02002d1facf51ddd0100025fa1ae7b8e8db459
Bernhard Tellenbach <tebe@zhaw.ch>
2017-09-17 17:07:01 +0200

    Initial commit of the source files.

---
+++ src/Befehlswort.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Diese Klasse haelt eine Aufzaehlung der akzeptierten Befehlswoerter.
+ * Mit ihrer Hilfe werden eingetippte Befehle erkannt.
+ *
+ * @author  tebe
+ * @version 1.0
+ */
+public class Befehlswort {
+
+	// Ein HashSet mit den gueltigen Befehlswoertern als Klassenvariable
+	private static final HashSet<String> gueltigeBefehle = new HashSet<String>();
+	static {
+		gueltigeBefehle.add("gehe");
+		gueltigeBefehle.add("beenden");
+		gueltigeBefehle.add("hilfe");
+	}
+
+	/**
+	 * Konstruktor - initialisiere die Befehlswörter.
+	 */
+	public Befehlswort()
+	{
+		// tut momentan nichts
+	}
+
+	/**
+	 * Pruefe, ob eine gegebene Zeichenkette ein gueltiger
+	 * Befehl ist.
+	 * 
+	 * @return 'true', wenn die gegebene Zeichenkette ein gueltiger
+	 *         Befehl ist, 'false' sonst.
+	 */
+	public static boolean istBefehl(String eingabe)
+	{
+		return gueltigeBefehle.contains(eingabe);
+	}
+
+	/**
+	 * Gibt die akzeptierten Befehlsworte als Text zurueck.
+	 * 
+	 * @return Die akzeptierten Befehlsworte als Text
+	 */
+	public static String gibBefehlsworteAlsText() {
+		String text = "";
+		for (String befehl : gueltigeBefehle) {
+			text = text + befehl + " ";
+		}
+		return text;
+	}
+}
